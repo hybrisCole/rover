0f267a31aa2258b1fe303fb66c39c50936943b33
Alberto Cole <acpii2005@gmail.com>
2014-06-23 00:19:28 -0600

    transiciones para ambos lados

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,29 +34,4 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  }).animation('.my-crazy-animation', function() {
-    return {
-      enter: function(element,done) {
-        $(element).velocity({left:'0'},
-          {
-            easing:'ease',
-            duration:150,
-            complete:function(){
-              done();
-            }
-          }
-        );
-      },
-      leave: function(element,done) {
-        $(element).velocity({left:'100%'},
-          {
-            easing:'ease',
-            duration:150,
-            complete:function(){
-              done();
-            }
-          }
-        );
-      }
-    };
   });
--- app/scripts/controllers/mother.js
+++ app/scripts/controllers/mother.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('roverApp')
-  .controller('MotherCtrl', function ($scope) {
+  .controller('MotherCtrl', function ($scope,$location) {
     $scope.val = false;
+    $scope.viewAnimation = 'next-animation';
+    $scope.goTo = function(location){
+      $scope.viewAnimation = 'next-animation';
+      $location.path(location);
+    };
+    $scope.goBack = function(location){
+      $scope.viewAnimation = 'previous-animation';
+      $location.path(location);
+    };
+
   });
