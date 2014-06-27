240ebd5fe9cb3abf7c36d905d9ffff256a06ae9c
Alberto Cole <acpii2005@gmail.com>
2014-06-26 22:02:34 -0600

    VIsta para accesorios

---
+++ app/scripts/controllers/accesorios.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roverAppApp.controller:AccesoriosCtrl
+ * @description
+ * # AccesoriosCtrl
+ * Controller of the roverApp
+ */
+angular.module('roverApp')
+  .controller('AccesoriosCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
