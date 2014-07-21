f283331aea895b6172e363ca8101eab9d4c152e1
Abdel Atencio <abdeldw@gmail.com>
2014-07-20 21:43:46 -0600

    safe pull

--- app/scripts/controllers/accesorio.js
+++ app/scripts/controllers/accesorio.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('roverApp')
-  .controller('AccesorioCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AccesorioCtrl', function ($scope,$cookieStore,$routeParams,vin) {
+    var vinNum = $cookieStore.get('vinNum');
+    var codigo = $routeParams.id;
+    vin.getUser(vinNum).then(function(user){
+      var model = user[0].modelo.replace(/\s/g, '').toLowerCase();
+      vin.getAcsry(model).then(function(data){
+        console.log(data);
+        $scope.accesorio = data;
+      });
+    });
   });
