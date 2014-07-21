7d3d3fdaa8a3cf4c6e015b6b92d5c2515697a963
Abdel Atencio <abdeldw@gmail.com>
2014-07-20 21:30:03 -0600

    safe pull

--- app/scripts/controllers/historial.js
+++ app/scripts/controllers/historial.js
@@ -6,8 +6,8 @@ angular.module('roverApp')
     vin.getUser(vinNum).then(function(user){
       $scope.historial = _.map(user[0].historial, function(hist){
       	var obj = {
-      		'fecha' : $scope.proximaFecha = moment(hist.fecha).format('DD MMMM YYYY'),
-      		'km'    : hist.km,
+      		'fecha'  : $scope.proximaFecha = moment(hist.fecha).format('DD MMMM YYYY'),
+      		'km'     : hist.km,
       		'list'   : hist.listado
       	};
       	return obj;
