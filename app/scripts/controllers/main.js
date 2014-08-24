345ca97e19d25fb469f056b43f14604300cb4e77
Alberto Cole <acpii2005@gmail.com>
2014-08-24 09:56:24 -0600

    Agregando informacion de la vista de perfil al login en firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the roverApp
  */
 angular.module('roverApp')
-  .controller('MainCtrl', function ($scope,$location,vin){
+  .controller('MainCtrl', function ($scope,$location,vin,firebaseService){
   	$scope.vin = '';
 
     $scope.sendVin = function(){
@@ -17,6 +17,15 @@ angular.module('roverApp')
           if(!_.isEmpty(data)){
             localStorage.setItem('vinNum',$scope.vin);
             $scope.viewAnimation = 'slide-velocity-next';
+            firebaseService.getPerfilInfo($scope.vin)
+              .then(function(perfilInfo){
+                if(perfilInfo.email === ''){
+                  firebaseService.setPerfilInfo($scope.vin,{
+                    email:data.correo,
+                    celular:data.telefono
+                  });
+                }
+            });
             $location.path('/home');
           }else{
             $scope.errorVin = 'Su VIN no se encuentra registrado';
